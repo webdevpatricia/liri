4078a31d9743cc8a3e94f7f0cf608236e459361f
Pat Carlson <webdevpatricia@gmail.com>
2018-01-19 23:07:27 -0800

    got twitter working and auth module working

--- assets/javascript/keys.js
+++ assets/javascript/keys.js
@@ -1,17 +1,14 @@
-console.log('this is loaded');
 
-var twitterKeys = {
+
+exports.twitterKeys = {
   consumer_key: '4SZeAfd9h82UVm3e2EG1H1d19',
   consumer_secret: 'E7dyIz4ONVsDCDeY4jWJ9SYg15f2awTB7pckOeWAYyvgMsfWbD',
   access_token_key: '951567798920126465-pkGa1kZUXtwQz0nZjO8FE8PZrUSlkJb',
-  access_token_secret: 'yb8iae2LZ4R0VQadph7oVUHTyMZt71yuPpGza6yG44NFh',
-}
+  access_token_secret: 'yb8iae2LZ4R0VQadph7oVUHTyMZt71yuPpGza6yG44NFh'
+};
 
-module.exports = twitterKeys;
-
-var spotifyKeys = {
-	client_key: 'd3e88c6592204e69a66358f79bd498d0';
+exports.spotifyKeys = {
+	client_key: 'd3e88c6592204e69a66358f79bd498d0',
 	client_secret: '8cfa6ac2849745a3b3a4ee45caa4e894'
 }
 
-module.exports = spotifyKeys;
